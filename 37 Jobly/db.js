fefc330d60533600b766830191d98f3e411f9eca
Anthony R [Oct 22] <116580403+KingLemmiwinks@users.noreply.github.com>
2023-05-22 14:07:23 -0400

    Initial Commit
    100% of tests working

---
+++ 37 Jobly/db.js
@@ -0,0 +1,14 @@
+
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+
+let db = new Client({
+  host: "localhost",
+  database: "jobly",
+  password: "3Turtlz6!",
+});
+
+db.connect();
+
+module.exports = db;
